8c53df420e802070b1818c89e7bf59ee659559cc
echoPacheco <mateuspacheco2201@gmail.com>
2024-01-12 14:14:38 -0800

    footer (partial)

--- src/views/CarHome.vue
+++ src/views/CarHome.vue
@@ -7,7 +7,7 @@
     </div>
   </container>
 
-  <container class="container">
+  <div class="container">
     <div id="popularCars" class="carousel slide" data-bs-ride="carousel">
       <div class="carousel-indicators">
         <button type="button" data-bs-target="#popularCars" data-bs-slide-to="0" class="active" aria-current="true"
@@ -53,7 +53,7 @@
         <span class="visually-hidden">Next</span>
       </button>
     </div>
-  </container>
+  </div>
 
   <container>
   <div class="company">
@@ -95,6 +95,22 @@
   </div>
 </container>
 
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+    </ul>
+  </footer>
+</div>
 
 </template>
   

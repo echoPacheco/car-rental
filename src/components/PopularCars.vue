b4fa998ec23aa300b6deac089988f8cb697d5994
echoPacheco <mateuspacheco2201@gmail.com>
2024-01-22 12:14:03 -0800

    changes on popular cars

--- src/components/PopularCars.vue
+++ src/components/PopularCars.vue
@@ -10,8 +10,11 @@
       <div class="carousel-item active">
         <img :src="require('@/assets/' + popularCars[0].img)" class="carousel-img" alt="...">
         <div class="carousel-content">
-          <h1>{{ this.popularCars[0].name }}</h1>
-          <p>{{ this.popularCars[0].price }}</p>
+          <h5 class="card-title fs-1">{{ this.popularCars[0].name }}</h5>
+          <p class="flex mt-6 fs-4">
+            <span class="self-start fs-6 ">$</span> {{ this.popularCars[0].price }}
+            <span class="self-end fs-6 fw-medium">/day</span>
+          </p>
 
           <div class="carousel-icons-container">
             <div class="icon-text-container">
@@ -31,7 +34,7 @@
             </div>
 
             <div class="icon-text-container">
-              <img src="@/assets/transmission_light.png" class="carousel-icons">
+              <img src="@/assets/steering-wheel.svg" class="carousel-icons">
               <p>{{ this.popularCars[0].transmission }}</p>
             </div>
           </div>
@@ -42,9 +45,11 @@
       <div class="carousel-item">
         <img :src="require('@/assets/' + popularCars[1].img)" class="carousel-img" alt="...">
         <div class="carousel-content">
-          <h1>{{ this.popularCars[1].name }}</h1>
-          <p>{{ this.popularCars[1].price }}</p>
-
+          <h5 class="card-title fs-1">{{ this.popularCars[1].name }}</h5>
+          <p class="flex mt-6 fs-4">
+            <span class="self-start fs-6 ">$</span> {{ this.popularCars[1].price }}
+            <span class="self-end fs-6 fw-medium">/day</span>
+          </p>
           <div class="carousel-icons-container">
             <div class="icon-text-container">
               <img src="@/assets/gas_pump_light.png" class="carousel-icons">
@@ -63,7 +68,7 @@
             </div>
 
             <div class="icon-text-container">
-              <img src="@/assets/transmission_light.png" class="carousel-icons">
+              <img src="@/assets/steering-wheel.svg" class="carousel-icons">
               <p>{{ this.popularCars[1].transmission }}</p>
             </div>
           </div>
@@ -74,9 +79,11 @@
       <div class="carousel-item">
         <img :src="require('@/assets/' + popularCars[2].img)" class="carousel-img" alt="...">
         <div class="carousel-content">
-          <h1>{{ this.popularCars[2].name }}</h1>
-          <p>{{ this.popularCars[2].price }}</p>
-
+          <h5 class="card-title fs-1">{{ this.popularCars[2].name }}</h5>
+          <p class="flex mt-6 fs-4">
+            <span class="self-start fs-6 ">$</span> {{ this.popularCars[2].price }}
+            <span class="self-end fs-6 fw-medium">/day</span>
+          </p>
           <div class="carousel-icons-container">
             <div class="icon-text-container">
               <img src="@/assets/gas_pump_light.png" class="carousel-icons">
@@ -95,7 +102,7 @@
             </div>
 
             <div class="icon-text-container">
-              <img src="@/assets/transmission_light.png" class="carousel-icons">
+              <img src="@/assets/steering-wheel.svg" class="carousel-icons">
               <p>{{ this.popularCars[2].transmission }}</p>
             </div>
           </div>
@@ -103,7 +110,7 @@
           <router-link to="/cars" type="button" class="btn btn-outline-light mt-4" style="width: 30%;">Book</router-link>
         </div>
       </div>
-      </div>
+    </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#popularCars" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Previous</span>

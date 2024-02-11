9f56fcb62e62ea3cbf07386d67acd497d431d3d6
echoPacheco <mateuspacheco2201@gmail.com>
2024-02-11 02:24:35 -0800

    checkout view (partial)

--- src/views/CheckoutDetail.vue
+++ src/views/CheckoutDetail.vue
@@ -1,263 +1,318 @@
 <template>
-    <div>
-        <h1>Checkout</h1>
+  <div class="container">
 
-        <div class="track">
-            <div v-for="step in steps" :key="step"
-                :class="{ 'step': true, 'active': currentStep === step, 'completed': completedSteps.includes(step) }">
-                <span class="icon"></span>
-                <span class="text">{{ stepTitles[step - 1] }}</span>
-            </div>
+    <div class="track-container">
+      <div class="track">
+        <div v-for="step in steps" :key="step" :class="{
+          step: true,
+          active: currentStep === step,
+          completed: completedSteps.includes(step),
+        }">
+          <span class="icon"></span>
+          <span class="text">{{ stepTitles[step - 1] }}</span>
         </div>
-
-        <div>
-            <div v-show="currentStep === 1">
-                <div v-if="parsedCarImage">
-                    <img :src="parsedCarImage" class="card-img-top img-fluid"
-                        style="max-height: 250px; max-width: fit-content; object-fit: cover;" alt="">
-                </div>
-                <div v-if="parsedCarData">
-                    <div v-for="(value, key) in parsedCarData" :key="key">
-                        <p>{{ key }}: {{ value }}</p>
-                    </div>
-                </div>
-            </div>
-            <div v-show="currentStep === 2">
-                <datepicker v-model="selectedDate" format="yyyy-MM-dd" placeholder="Select a date"></datepicker>
-            </div>
-            <div v-show="currentStep === 3">Third Part</div>
-            <div v-show="currentStep === 4">Fourth Part</div>
-        </div>
-
-        <div>
-            <button @click="decreaseStep" class="btn btn-outline-light me-2" :disabled="currentStep === 1">Decrease
-                Step</button>
-            <button @click="increaseStep" class="btn btn-outline-light me-2" :disabled="currentStep === 4">Increase
-                Step</button>
-        </div>
+      </div>
     </div>
+
+    <h1 class="p-2">Checkout</h1>
+    <div class="justify-content-center ">
+      <div class="text-center">
+
+        <div v-show="currentStep === 1">
+          <div v-if="parsedCarImage && parsedCarData">
+            <CheckoutCar :carData="parsedCarData" :carImage="parsedCarImage" />
+          </div>
+        </div>
+
+        <div v-show="currentStep === 2">
+          <CheckoutPayment :carData="parsedCarData" :carImage="parsedCarImage" />
+        </div>
+        
+        <div v-show="currentStep === 3">
+          <!-- Your content for step 3 -->
+        </div>
+        <div v-show="currentStep === 4">
+          <h2 class="text-center">Confirmar e Finalizar</h2>
+          <!-- Adicione campos e lógica para confirmação final -->
+          <button @click="saveCheckoutData" class="btn btn-warning">
+            Confirmar e Finalizar
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center mt-3">
+      <div class="col-md-6 text-center">
+        <button @click="decreaseStep" class="btn btn-outline-light me-2" v-if="currentStep !== 1">
+          Back
+        </button>
+        <button @click="increaseStep" class="btn btn-outline-light me-2" :disabled="currentStep === 4">
+          Next
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { ref, onMounted } from "vue";
-import { useRoute } from 'vue-router';
-import Datepicker from 'vue-datepicker'
+import { useRoute } from "vue-router";
+import CheckoutCar from "@/components/CheckoutCar.vue";
+import CheckoutPayment from "@/components/CheckoutPayment.vue";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/main"
+
+// import { getAuth } from "firebase/auth";
 
 export default {
-    props: {
-        carData: String,
-        carImage: String,
-    },
-    components: {
-        Datepicker,
-    },
-    setup(props) {
-        const route = useRoute();
-        const parsedCarData = ref(null);
-        const parsedCarImage = ref(null);
-        const currentStep = ref(1);
-        const completedSteps = ref([]);
-        const steps = [1, 2, 3, 4];
-        const stepTitles = ['Review', 'Date', 'Payment', 'Ready for pickup'];
-        const selectedDate = ref(new Date());
+  props: {
+    carData: String,
+    carImage: String,
+  },
+  components: { CheckoutCar, CheckoutPayment },
+  setup(props) {
+    const route = useRoute();
+    const parsedCarData = ref(null);
+    const parsedCarImage = ref(null);
+    const currentStep = ref(1);
+    const completedSteps = ref([]);
+    const steps = [1, 2, 3, 4];
+    const stepTitles = ["Review", "Payment", "Confirm", "Ready for pickup"];
+    const value = ref(null);
+    const defaultTime = ref(null);
+    const darkTheme = ref("dark");
 
-        onMounted(() => {
-            if (props.carData) {
-                const decodedData = decodeURIComponent(props.carData);
-                parsedCarData.value = JSON.parse(decodedData);
-            }
+    onMounted(() => {
+      if (props.carData) {
+        const decodedData = decodeURIComponent(props.carData);
+        parsedCarData.value = JSON.parse(decodedData);
+      }
 
-            parsedCarImage.value = props.carImage || route.params.carImage;
-        });
+      parsedCarImage.value = props.carImage || route.params.carImage;
+    });
 
-        const increaseStep = () => {
-            if (currentStep.value < 4) {
-                completedSteps.value.push(currentStep.value);
-                currentStep.value += 1;
-            }
-        };
+    const increaseStep = () => {
+      if (currentStep.value < 4) {
+        completedSteps.value.push(currentStep.value);
+        currentStep.value += 1;
+      }
+    };
 
-        const decreaseStep = () => {
-            if (currentStep.value > 1) {
-                completedSteps.value.pop();
-                currentStep.value -= 1;
-            }
-        };
+    const decreaseStep = () => {
+      if (currentStep.value > 1) {
+        completedSteps.value.pop();
+        currentStep.value -= 1;
+      }
+    };
 
-        return {
-            parsedCarData,
-            parsedCarImage,
-            currentStep,
-            completedSteps,
-            steps,
-            stepTitles,
-            increaseStep,
-            decreaseStep,
-            selectedDate,
-        };
-    },
+    const saveCheckoutData = async () => {
+      const querySnapshot = await getDocs(collection(db, "rents"))
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+      })
+
+    };
+
+    // const saveCheckoutData = async () => {
+    //   const user = auth.currentUser;
+
+    //   if (!user) {
+    //     console.error("Usuário não autenticado. Não é possível salvar dados do checkout.");
+    //     return;
+    //   }
+
+    //   const checkoutCollection = collection(db, 'rents');
+
+    //   const checkoutData = {
+    //     make: "audi",
+    //     model: "a6",
+    //     img: "https://cdn.imagin.studio/getimage?customer=hrjavascript-mastery&make=audi&modelFamily=a3&zoomType=fullscreen&modelYear=2023",
+    //     price: 100.50,
+    //     start_date: "15/02/2024",
+    //     end_date: "16/02/2024",
+    //     userId: user.uid,
+    //     timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    //   };
+
+    //   try {
+    //     const docRef = await addDoc(checkoutCollection, checkoutData);
+    //     console.log("Dados do checkout adicionados com sucesso! ID do documento:", docRef.id);
+    //   } catch (error) {
+    //     console.error("Erro ao adicionar dados do checkout:", error);
+    //   }
+    // };
+
+    return {
+      parsedCarData,
+      parsedCarImage,
+      currentStep,
+      completedSteps,
+      steps,
+      stepTitles,
+      increaseStep,
+      decreaseStep,
+      value,
+      defaultTime,
+      darkTheme,
+      saveCheckoutData,
+    };
+  },
 };
 </script>
 
 
 <style>
 .container {
-    margin-top: 50px;
-    margin-bottom: 50px
+  margin-top: 50px;
+  margin-bottom: 50px;
 }
 
-.card {
-    position: relative;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    min-width: 0;
-    word-wrap: break-word;
-    background-color: #fff;
-    background-clip: border-box;
-    border: 1px solid rgba(0, 0, 0, 0.1);
-    border-radius: 0.10rem
-}
-
-.card-header:first-child {
-    border-radius: calc(0.37rem - 1px) calc(0.37rem - 1px) 0 0
-}
-
-.card-header {
-    padding: 0.75rem 1.25rem;
-    margin-bottom: 0;
-    background-color: #fff;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.1)
+.track-container {
+  border-top: #222529 solid 2px;
+  border-bottom: #222529 solid 2px;
+  margin-bottom: 30px;
 }
 
 .track {
-    position: relative;
-    background-color: #ddd;
-    height: 7px;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    margin-bottom: 60px;
-    margin-top: 50px
+  position: relative;
+  background-color: #ddd;
+  height: 7px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  margin-bottom: 60px;
+  margin-top: 50px;
 }
 
 .track .step {
-    -webkit-box-flex: 1;
-    -ms-flex-positive: 1;
-    flex-grow: 1;
-    width: 25%;
-    margin-top: -18px;
-    text-align: center;
-    position: relative
+  -webkit-box-flex: 1;
+  -ms-flex-positive: 1;
+  flex-grow: 1;
+  width: 25%;
+  margin-top: -18px;
+  text-align: center;
+  position: relative;
 }
 
 .track .step.active:before {
-    background: #0d6efd
+  background: #0d6efd;
 }
 
 .track .step::before {
-    height: 7px;
-    position: absolute;
-    content: "";
-    width: 100%;
-    left: 0;
-    top: 18px
+  height: 7px;
+  position: absolute;
+  content: "";
+  width: 100%;
+  left: 0;
+  top: 18px;
 }
 
 .track .step.active .icon {
-    background: #0d6efd;
-    color: #fff
+  background: #0d6efd;
+  color: #fff;
 }
 
 .track .icon {
-    display: inline-block;
-    width: 40px;
-    height: 40px;
-    line-height: 40px;
-    position: relative;
-    border-radius: 100%;
-    background: #ddd
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  line-height: 40px;
+  position: relative;
+  border-radius: 100%;
+  background: #ddd;
 }
 
 .track .step.active .text {
-    font-weight: 400;
+  font-weight: 400;
 }
 
 .track .text {
-    display: block;
-    margin-top: 7px
+  display: block;
+  margin-top: 7px;
 }
 
 .itemside {
-    position: relative;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    width: 100%
+  position: relative;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  width: 100%;
 }
 
 .itemside .aside {
-    position: relative;
-    -ms-flex-negative: 0;
-    flex-shrink: 0
+  position: relative;
+  -ms-flex-negative: 0;
+  flex-shrink: 0;
 }
 
 .img-sm {
-    width: 80px;
-    height: 80px;
-    padding: 7px
+  width: 80px;
+  height: 80px;
+  padding: 7px;
 }
 
 ul.row,
 ul.row-sm {
-    list-style: none;
-    padding: 0
+  list-style: none;
+  padding: 0;
 }
 
 .itemside .info {
-    padding-left: 15px;
-    padding-right: 7px
+  padding-left: 15px;
+  padding-right: 7px;
 }
 
 .itemside .title {
-    display: block;
-    margin-bottom: 5px;
-    color: #212529
+  display: block;
+  margin-bottom: 5px;
+  color: #212529;
 }
 
 p {
-    margin-top: 0;
-    margin-bottom: 1rem
+  margin-top: 0;
+  margin-bottom: 1rem;
 }
 
 .btn-warning {
-    color: #ffffff;
-    background-color: #ee5435;
-    border-color: #ee5435;
-    border-radius: 1px
+  color: #ffffff;
+  background-color: #ee5435;
+  border-color: #ee5435;
+  border-radius: 1px;
 }
 
 .btn-warning:hover {
-    color: #ffffff;
-    background-color: #ff2b00;
-    border-color: #ff2b00;
-    border-radius: 1px
+  color: #ffffff;
+  background-color: #ff2b00;
+  border-color: #ff2b00;
+  border-radius: 1px;
 }
 
 .track .step.completed:before {
-    background: #0d6efd
+  background: #0d6efd;
 }
 
 .track .step.completed .icon {
-    background: #0d6efd;
-    color: #fff;
+  background: #0d6efd;
+  color: #fff;
 }
 
+.demo-date-picker {
+  display: flex;
+  width: 100%;
+  padding: 0;
+  flex-wrap: wrap;
+}
 
+.demo-date-picker .block {
+  padding: 30px 0;
+  text-align: center;
+  border-right: solid 1px var(--el-border-color);
+  flex: 1;
+}
+
+.demo-date-picker .block:last-child {
+  border-right: none;
+}
 
 /* .row {
     display: flex;
